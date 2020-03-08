9c0636bd37e9209ab04949e612b28ba9c5dcf6e9
YuTin <yuting1987@gmail.com>
2020-03-08 17:54:43 +0800

    ci(deploy): update kustomize configs

--- server/app/healthz.js
+++ server/app/healthz.js
@@ -1,5 +1,7 @@
+import cors from "cors";
+
 export default ({ app }) => {
-  app.get("/healthz", (req, res) => {
+  app.get("/healthz", cors(), (req, res) => {
     res.status(200).send("All systems operational");
   });
 };
